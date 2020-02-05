69897a07044e9ae3af029b96da9ec03f9c60c0e5
ningchenqing <18974159998@163.com>
2020-02-05 21:42:35 +0800

    后台2.5号的代码

--- y2ThirdXM/后台代码/y2/src/main/java/com/accp/service/NcqzzjgdyService.java
+++ y2ThirdXM/后台代码/y2/src/main/java/com/accp/service/NcqzzjgdyService.java
@@ -1,11 +1,15 @@
 package com.accp.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.accp.domain.Bumen;
+import com.accp.domain.Gangwei;
 import com.accp.mapper.BumenMapper;
+import com.accp.mapper.GangweiMapper;
 
 @Service
 @Transactional
@@ -14,9 +18,22 @@ public class NcqzzjgdyService {
 	@Autowired
 	BumenMapper bumenMapper;
 	
-	/*
-	 * public Bumen querybm() { return bumenMapper.selectByPrimaryKey(null); }
-	 */
+	@Autowired
+	GangweiMapper gangweiMapper;
 	
+	//组织机构树状图查询
+//	public Bumen querybm() {
+//		return bumenMapper.selectByPrimaryKey(null);
+//	}
+	
+	//岗位定义查询
+	public List<Gangwei> querygw(){
+		return gangweiMapper.selectByExample(null);
+	}
+	
+	//新增岗位
+	public int insertgw(Gangwei gangwei) {
+		return gangweiMapper.insert(gangwei);
+	}
 	
 }
